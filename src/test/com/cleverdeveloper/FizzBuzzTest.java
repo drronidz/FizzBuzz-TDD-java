5d3ce03f8d64605607ecccce7732e67fee50cd45
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-06 23:33:57 +0100

    Refactoring ...

--- src/test/com/cleverdeveloper/FizzBuzzTest.java
+++ src/test/com/cleverdeveloper/FizzBuzzTest.java
@@ -9,25 +9,33 @@ DATE : 1/6/2022 11:06 PM
 */
 
 import main.java.com.cleverdeveloper.FizzBuzz;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     public void shouldReturn1IfNumberIs1() {
-        assertEquals("1", new FizzBuzz().generate(1));
+        assertEquals("1", fizzBuzz.generate(1));
     }
 
     @Test
     public void shouldReturn2IfNumberIs2() {
-        assertEquals("2", new FizzBuzz().generate(2));
+        assertEquals("2", fizzBuzz.generate(2));
     }
 
     @Test
     public void shouldReturnFizzIfNumberIs3() {
-        assertEquals("Fizz", new FizzBuzz().generate(3));
+        assertEquals("Fizz", fizzBuzz.generate(3));
     }
 
 

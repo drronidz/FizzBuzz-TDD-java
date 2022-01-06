f26d3c264f44cd1c0d3cf217718ce3681cc88378
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-06 23:29:54 +0100

    1- Creating project classes ... 2- Starting with the first two cases ...

--- src/main/java/com/cleverdeveloper/FizzBuzz.java
+++ src/main/java/com/cleverdeveloper/FizzBuzz.java
@@ -7,5 +7,15 @@ Author Name : @ DRRONIDZ
 DATE : 1/6/2022 11:02 PM
 */
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class FizzBuzz {
+
+    public String generate(int number) {
+        return String.valueOf(number);
+    }
+
 }
--- src/test/com/cleverdeveloper/FizzBuzzTest.java
+++ src/test/com/cleverdeveloper/FizzBuzzTest.java
@@ -8,6 +8,22 @@ Author Name : @ DRRONIDZ
 DATE : 1/6/2022 11:06 PM
 */
 
+import main.java.com.cleverdeveloper.FizzBuzz;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FizzBuzzTest {
+
+    @Test
+    public void shouldReturn1IfNumberIs1() {
+        assertEquals("1", new FizzBuzz().generate(1));
+    }
+
+    @Test
+    public void shouldReturn2IfNumberIs2() {
+        assertEquals("2", new FizzBuzz().generate(2));
+    }
+
 
 }
